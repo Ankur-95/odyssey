de4465ecd377315208e326ee8e3a5286a0000b4a
Ankur-95 <ankurujawane@gmail.com>
2026-05-21 22:41:12 +0530

    Some Lil changes

--- phase_1/week_1/day_1/hello.cpp
+++ phase_1/week_1/day_1/hello.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+// The iostream header provides input/output capabilities
+// It stands for "Input/Output String Stream."
+// it replaces the C's <stdio.h>
 
+// This is the mainn function where the execution of the program begins
 int main()
 {
-    // Print a simple message to ensure the iostream header is used
+    // std:: cout is the c++ way to print text on the screen.
+    //  It stands for " Standard Character output".
+    //  It replaces C's "print f" function
+
+    // The "<<" symbols are insertion operators.
+    // They "push" your text into the output stream.
+
+    // std::endl stands for "End Line".
+    // It creates a new line, replacing C's "\n" character.
+
     std::cout << "Hello, World!" << std::endl;
+    // Signals to the operating system that the program finished successfully.    return 0;
     return 0;
 }
